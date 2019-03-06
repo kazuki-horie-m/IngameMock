6f456374f25814feab1635cd5cf513c24a6fc470
Kazuki Horie <kazuki.horie.ts@mixi.co.jp>
2019-03-06 14:45:33 +0900

    layout ws view

--- IngameMock/WebSocket/WSTopViewController.swift
+++ IngameMock/WebSocket/WSTopViewController.swift
@@ -9,5 +9,26 @@
 import UIKit
 
 class WSTopViewController: UIViewController {
+    @IBOutlet private weak var tfAddress: UITextField?
+    @IBOutlet private weak var tvLog: UITextView?
     
+    @IBAction private func btnHello(sender: UIButton) {
+        
+    }
+    
+    @IBAction private func btnBang(sender: UIButton) {
+        
+    }
+    
+    @IBAction private func btnBye(sender: UIButton) {
+        
+    }
+    
+    @IBAction private func btnConnect(sender: UIButton) {
+        
+    }
+    
+    @IBAction private func btnDisconnect(sender: UIButton) {
+        
+    }
 }
