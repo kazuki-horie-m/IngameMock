4836a6a584bdf6c5fb000c57148f36480444dba2
Kazuki Horie <kazuki.horie.ts@mixi.co.jp>
2019-03-07 14:18:17 +0900

    add signaling server

--- IngameMock/WebRTC/WRTopViewController.swift
+++ IngameMock/WebRTC/WRTopViewController.swift
@@ -9,14 +9,19 @@
 import UIKit
 import WebRTC
 import Starscream
+import AVFoundation
 
 class WRTopViewController: UIViewController {
-    @IBOutlet private weak var videoView: RTCEAGLVideoView?
+    @IBOutlet private weak var videoView: RTCEAGLVideoView!
     @IBOutlet private weak var cameraView: RTCCameraPreviewView?
     
     var websocket: WebSocket?
     
-    @IBAction func closeButtonAction(_ sender: Any) {
+    @IBAction func connectButtonAction(_ sender: UIButton) {
+        soranohito()
+    }
+    
+    @IBAction func closeButtonAction(_ sender: UIButton) {
         // Closeボタンを押した時
         websocket?.disconnect()
         _ = self.navigationController?.popToRootViewController(animated: true)
@@ -25,12 +30,71 @@ class WRTopViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        websocket = WebSocket(url: URL(string:
-            "wss://conf.space/WebRTCHandsOnSig/tctc13")!)
-        websocket?.delegate = self
-        websocket?.connect()
+        soranohito()
     }
     
+    
+    let pcFactory = RTCPeerConnectionFactory()
+    var localMediaStream: RTCMediaStream?
+    var peer: RTCPeerConnection?
+    
+    private func soranohito() {
+        let constraints = RTCMediaConstraints(
+            mandatoryConstraints: ["OfferToReceiveVideo": kRTCMediaConstraintsValueTrue,
+                                   "OfferToReceiveAudio": kRTCMediaConstraintsValueTrue],
+            optionalConstraints: nil)
+        let peerConn = pcFactory.peerConnection(with: RTCConfiguration(),
+                                              constraints: constraints,
+                                              delegate: nil)
+        
+        // 映像トラック
+        let videoSource = pcFactory.videoSource()
+        let videoTrack = pcFactory.videoTrack(with: videoSource,
+                                              trackId: "video1")
+        
+        // 音声トラック
+        let audioSource = pcFactory.audioSource(with: nil)
+        let audioTrack = pcFactory.audioTrack(with: audioSource,
+                                              trackId: "audio1")
+        
+        // ストリーム
+        let stream = pcFactory.mediaStream(withStreamId: "stream1")
+        stream.addVideoTrack(videoTrack)
+        stream.addAudioTrack(audioTrack)
+        
+        // ストリームの追加
+        peerConn.add(stream)
+        
+        videoTrack.add(videoView)
+    }
+    
+    private func noServerStart() {
+//        peer = [pc_factory peerConnectionWithICEServers:nil constraints:nil delegate:nil];
+//        peer = pcFactory.peerConnection(with: <#T##RTCConfiguration#>, constraints: <#T##RTCMediaConstraints#>, delegate: <#T##RTCPeerConnectionDelegate?#>)
+        
+        // local_video_view = videoView
+        
+        localMediaStream = pcFactory.mediaStream(withStreamId: "ARDAMS")
+        
+        var cameraId: String?
+        
+        for dev in AVCaptureDevice.devices(for: .video) {
+            if dev.position == .front {
+                cameraId = dev.localizedName
+            }
+        }
+        
+//        RTCVideoCapturer *capturer = [RTCVideoCapturer capturerWithDeviceName:camera_id];
+        let capturer = RTCVideoCapturer(delegate: self)
+//        RTCVideoSource *source     = [pc_factory videoSourceWithCapturer:capturer constraints:nil];
+        let source: RTCVideoSource = pcFactory.videoSource()
+        
+//        RTCVideoTrack *video_track = [pc_factory videoTrackWithID:@"ARDAMSv0" source:source];
+        let videoTrack = pcFactory.videoTrack(with: source, trackId: "ARDAMSv0")
+        
+        localMediaStream?.addVideoTrack(videoTrack)
+        videoTrack.add(videoView)
+    }
     
     func LOG(_ body: String = "",
              function: String = #function,
@@ -39,6 +103,12 @@ class WRTopViewController: UIViewController {
         print("[\(function) : \(line)] \(body)")
     }
     
+}
+
+extension WRTopViewController: RTCVideoCapturerDelegate {
+    func capturer(_ capturer: RTCVideoCapturer, didCapture frame: RTCVideoFrame) {
+    
+    }
 }
 
 extension WRTopViewController: WebSocketDelegate {
