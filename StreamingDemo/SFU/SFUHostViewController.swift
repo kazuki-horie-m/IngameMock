ed8d3aa3572aa76564f09067dd5c25cfc605ac11
Kazuki Horie <kazuki.horie.ts@mixi.co.jp>
2019-03-12 11:59:04 +0900

    add SFU ViewControllers

---
+++ StreamingDemo/SFU/SFUHostViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SFUHostViewController.swift
+//  StreamingDemo
+//
+//  Created by kazuki.horie.ts on 2019/03/12.
+//  Copyright © 2019年 Kazuki Horie. All rights reserved.
+//
+
+import UIKit
+
+class SFUHostViewController: UIViewController {
+    
+}
