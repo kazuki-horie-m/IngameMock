f2aa3fcfb9dd49d21db80cca9ee77d12b54bddc0
Kazuki Horie <kazuki.horie.ts@mixi.co.jp>
2019-03-13 19:03:34 +0900

    add vois stamp view

---
+++ StreamingDemo/VoiceStamp/VoiceStampViewController.swift
@@ -0,0 +1,14 @@
+//
+//  VoiceStampViewController.swift
+//  StreamingDemo
+//
+//  Created by kazuki.horie.ts on 2019/03/13.
+//  Copyright © 2019年 Kazuki Horie. All rights reserved.
+//
+
+import UIKit
+import CoreML
+
+class VoiceStampViewController: UIViewController {
+    @IBOutlet private weak var outputLabel: UILabel!
+}
