ad10d40eea979d6aaccf7d7e4665753282512caa
Kazuki Horie <kazuki.horie.ts@mixi.co.jp>
2019-03-28 20:34:20 +0900

    Xcode 10.2

---
+++ StreamingDemo/OpenEars/ModalyViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ModaryViewController.swift
+//  StreamingDemo
+//
+//  Created by kazuki.horie.ts on 2019/03/28.
+//  Copyright © 2019 Kazuki Horie. All rights reserved.
+//
+
+import Foundation
