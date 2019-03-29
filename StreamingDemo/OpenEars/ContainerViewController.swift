71797b1b13bfb89ba701f7a7e1d6e3740216460f
Kazuki Horie <kazuki.horie.ts@mixi.co.jp>
2019-03-29 16:04:11 +0900

    test modal,  container view controller

---
+++ StreamingDemo/OpenEars/ContainerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ContainerViewController.swift
+//  StreamingDemo
+//
+//  Created by kazuki.horie.ts on 2019/03/29.
+//  Copyright © 2019 Kazuki Horie. All rights reserved.
+//
+
+import UIKit
+
+class ContainerViewController: UIViewController {
+    @IBOutlet private weak var label: UILabel?
+    
+    @IBAction func dismissView(sender: UIButton) {
+        super.dismiss(animated: true, completion: nil)
+    }
+    
+    var identifier: String? {
+        didSet {
+            label?.text = identifier
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        label?.text = identifier
+    }
+    
+}
--- StreamingDemo/OpenEars/ModalyViewController.swift
+++ StreamingDemo/OpenEars/ModalyViewController.swift
@@ -1,9 +1,18 @@
 //
-//  ModaryViewController.swift
+//  ModalyViewController.swift
 //  StreamingDemo
 //
 //  Created by kazuki.horie.ts on 2019/03/28.
 //  Copyright © 2019 Kazuki Horie. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ModalyViewController: UIViewController {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let vc = segue.destination as? ContainerViewController
+            else { return }
+        
+        vc.identifier = segue.identifier
+    }
+}
